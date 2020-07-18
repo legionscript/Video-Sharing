e7a53520f40184fd8fb8f97faeded4c1b5ad361b
legionscript <legionscript@legionscript.com>
2020-07-18 16:59:42 -0400

    Tutorial 2

--- videos/views.py
+++ videos/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render, reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Video
 
-def index(request):
-	return render(request, 'videos/index.html')
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-class CreateVideo(CreateView):
+class Index(ListView):
+	model = Video
+	template_name = 'videos/index.html'
+	order_by = '-date_posted'
+
+class CreateVideo(LoginRequiredMixin, CreateView):
 	model = Video
 	fields = ['title', 'description', 'video_file', 'thumbnail']
 	template_name = 'videos/create_video.html'
+
+	def form_valid(self, form):
+		form.instance.uploader = self.request.user
+		return super().form_valid(form)
 
 	def get_success_url(self):
 		return reverse('video-detail', kwargs={'pk': self.object.pk})
@@ -18,7 +27,7 @@ class DetailVideo(DetailView):
 	model = Video
 	template_name = 'videos/detail_video.html'
 
-class UpdateVideo(UpdateView):
+class UpdateVideo(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 	model = Video
 	fields = ['title', 'description']
 	template_name = 'videos/create_video.html'
@@ -26,9 +35,17 @@ class UpdateVideo(UpdateView):
 	def get_success_url(self):
 		return reverse('video-detail', kwargs={'pk': self.object.pk})
 
-class DeleteVideo(DeleteView):
+	def test_func(self):
+		video = self.get_object()
+		return self.request.user == video.uploader
+
+class DeleteVideo(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
 	model = Video
 	template_name = 'videos/delete_video.html'
 
 	def get_success_url(self):
 		return reverse('index')
+	
+	def test_func(self):
+		video = self.get_object()
+		return self.request.user == video.uploader
